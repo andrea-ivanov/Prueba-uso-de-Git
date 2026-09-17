b029883db93899dceb93cabbd081c46771a6e599
Alejandro Rivada Rodríguez <UO295528@uniovi.es>
2026-09-17 10:35:11 +0200

    prueba

---
+++ numeros.py
@@ -0,0 +1,11 @@
+# Pedimos el primer número al usuario
+num1 = float(input("Introduce el primer número: "))
+
+# Pedimos el segundo número al usuario
+#num2 = float(input("Introduce el segundo número: "))
+
+# Sumamos los dos números
+#resultado = num1 + num2
+
+# Mostramos el resultado en la pantalla
+print(f"El resultado de la suma es: {resultado}")
